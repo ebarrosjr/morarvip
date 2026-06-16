1c5c77887d1ce6e78852d6f7c195e496341b1988
Everton C B jr <ebarrosjr@gmail.com>
2026-06-16 17:12:31 -0300

    feat: AD financiar

--- site/templates/Index/index.php
+++ site/templates/Index/index.php
@@ -2,7 +2,7 @@
 <?= $this->Html->css('listagem') ?>
 <?= $this->element('property_results', [
     'imoveis' => $imoveis,
-    'headerContent' => '<div class="advertinsig"><span>PUBLICIDADE</span></div>',
+    'headerContent' => '<div class="advertinsig"><span>PUBLICIDADE</span><img src="https://imagens.morar.vip/main/images/financiar_202606161700.png" alt="Publicidade"></div>',
 ]) ?>
 <?= $this->element('property_filters', [
     'tipoimoveis' => $tipoimoveis,
