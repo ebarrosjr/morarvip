44be935f2f375e0b2d216725f577024053a2f121
Everton C B jr <ebarrosjr@gmail.com>
2026-06-18 09:44:51 -0300

    feat: Dash init

--- site/templates/Users/dashboard.php
+++ site/templates/Users/dashboard.php
@@ -1,5 +1,7 @@
 <pre>
+    Você está logado como: <?= $this->request->getAttribute('identity')->get('nome') ?>
+    <a href="<?= $this->Url->build(['controller' => 'Users', 'action' => 'logout']) ?>">Sair</a>
 <?php
-var_dump($this->request->getAttribute('identity'));
+// var_dump($this->request->getAttribute('identity'));
 ?>
 </pre>
