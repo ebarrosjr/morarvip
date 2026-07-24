c7af5e9cdffb470b0709a9ffa4f75b2102115746
Everton C B jr <ebarrosjr@gmail.com>
2026-07-24 10:42:55 -0300

    fix: Termos de Uso, Erick

--- site/templates/Index/termos_de_uso.php
+++ site/templates/Index/termos_de_uso.php
@@ -1,7 +1,7 @@
 <div class="col-12">
-    <h2 class="mt-3">Termos de Uso do morar.vip</h2>
-    <p>Bem-vindo ao morar.vip! Ao acessar e utilizar nosso site, você concorda com os seguintes termos de uso. Por favor, leia atentamente antes de prosseguir.</p>
-    <h3>1. Aceitação dos Termos</h3>
+    <h2 class="mt-3">Bem-vindo ao morar.vip!</h2>
+    <p>Ao acessar e utilizar nosso site, você concorda com os seguintes termos de uso. Por favor, leia atentamente antes de prosseguir.</p>
+    <h3>Aceitação dos Termos</h3>
     <p>Ao utilizar o morar.vip, você concorda em cumprir estes termos de uso e todas as leis e regulamentos aplicáveis. Se você não concorda com algum destes termos, por favor, não utilize nosso site.</p>
     <h3>2. Uso do Site</h3>
     <p>Você concorda em usar o morar.vip apenas para fins legais e de acordo com estes termos. É proibido utilizar o site para qualquer atividade que possa prejudicar, desativar ou sobrecarregar nossos serviços.</p>
@@ -11,7 +11,67 @@
     <p>O morar.vip não se responsabiliza por quaisquer danos diretos, indiretos ou consequenciais decorrentes do uso ou da incapacidade de uso do site. O acesso ao site é fornecido "no estado em que se encontra" e "conforme disponível".</p>
     <h3>5. Alterações nos Termos</h3>
     <p>Reservamo-nos o direito de modificar estes termos a qualquer momento. As alterações entrarão em vigor imediatamente após a publicação no site. Recomendamos que você revise os termos periodicamente.</p>
-    <h3>6. Contato</h3>
+    <div class="card">
+        <div class="card-body py-4 px-5">
+            <h3>Termos de uso</h3>
+            <h5>1 - Sua Conta</h5>
+            <p>Você só pode ter uma conta cadastrada no nosso Site.</p>
+            <p>Para cadastrar uma conta em nosso Site, você precisa ser maior de 18 anos. </p>
+            <p>1.2 - Criando a Conta: Ao criar uma conta no Site, você concorda que todas as informações fornecidas (“Informações da Conta”) são verdadeiras, assumindo sempre a responsabilidade de mantê-las atualizadas</p>
+            <p>Para que possamos proteger a sua conta, sua senha deve ser mantida em sigilo e não poderá ser compartilhada com ninguém. Todas as atividades realizadas aqui, serão sempre de sua responsabilidade. </p>
+            <p>1.3 - As “Informações da Conta”: servem para os potenciais compradores entrarem em contato com os usuários vendedores e, também permite que nós da M2U possamos enviar divulgações sobre promoções, pacotes, novidades ou outras informações sobre os serviços e sobre empresas do mesmo grupo.</p>
+            <p>Todos os e-mails enviados para seu endereço cadastrado serão considerados como entregues a Você. Você tem todo o direito de optar por deixar de receber tais e-mails, mas caso o cancelamento impossibilite a prestação dos serviços, a M2U reserva-se o direito de cancelar sua Conta. </p>
+            <p>1.4 - Suspensão e Exclusão da Conta: Use sua conta com consciência, pois tanto ela como seus anúncios poderão ser suspensos ou excluídos pela M2U caso as regras de utilização da plataforma sejam desrespeitadas. </p>
+            <p>1.5- Isso ocorrerá quando:</p>
+            <ul>
+                <li>Houver denúncia, e confirmação de mau uso: 
+                    <ul>
+                        <li>da conta ou do Site</li>
+                        <li>dos anúncios</li>
+                        <li>das funcionalidades do Site</li>
+                    </ul>
+                </li>
+                <li>Falsidade ou divergência nas informações da conta </li>
+                <li>Violação da legislação, da moral, da ordem pública, destes T&C e das Condições Particulares </li>
+                <li>Pela falta de pagamento em casos de funcionalidades pagas </li>
+            </ul>
+            <p>1.6- Consiste em mau uso e infração das regras de utilização da plataforma a utilização do Site para atividades ilícitas ou imorais, e qualquer atividade que, a critério da M2U, não esteja de acordo com suas políticas. </p>
+            <p class="my-3"><strong class="text-danger">ATENÇÃO: A M2U não é responsável por averiguar a veracidade das informações contidas nos anúncios. Qualquer informação inserida será de responsabilidade do usuário anunciante.</strong></p>
+            <p>1.7- Sua conta é pessoal e intransferível: Você não poderá compartilhar, transferir, vender ou alugar a sua conta para terceiros. A utilização da conta, as mensagens enviadas pelo Chat e os anúncios nela inseridos serão sempre de sua responsabilidade. CLIQUE PARA DESCOBRIR</p>
+            <p>1.7.1- Múltiplos Perfis: A identificação da utilização de múltiplos perfis ou o acesso/utilização da conta, em mais de um dispositivo simultaneamente, poderá acarretar a sua exclusão.</p>
+            <h5>2- Sobre os anúncios:</h5>
+            <p>2.1- Responsabilidade : Cabe exclusivamente a você a responsabilidade pelo anúncio, pelas informações contidas nele, pelas características e condições do imóvel ou serviço anunciado, pela sua postura como usuário, e pelos tributos que possam incidir na transação. </p>
+            <p>2.2- Para realizar o anúncio, você deverá preencher as informações doimóvel ou serviço, estado de conservação e demais informações solicitadas. Estas informações devem ser atualizadas sempre que ocorrerem situações que mudem as condições da oferta que possam interferir na negociação. Quanto mais completo o seu anúncio, melhores as chances de fechar negócio. </p>
+            <p>2.3- Somente poderão ser anunciados no Site, anúncios de imóveis.</p>   
+            <p>2.4- Quem pode anunciar: Qualquer pessoa natural ou empresa pode anunciar imóveis, mas para isso, é necessário ter autorização, procuração, posse mansa e pacífica e ou propriedade. Esta é uma obrigação.</p>
+            <p>2.5- Função Corretor Parceiro:</p>
+            <p>2.5.1- Na função Corretor Parceiro, o anunciante habilita por sua livre e espontânea vontade a possibilidade de receber solicitações de parcerias, e ao aceitá-las, deve ter ciência que está sub rogando (autorizando) para outros, o poder de exibir seus anúncios, em “espaços próprios” (url do corretor parceiro) dentro do domínio www.morarvip.com.br de outros corretores,. podendo a qualquer momento revogá-las .</p>
+            <p>2.5.2- A M2U não se responsabiliza por nenhum termo, acordo e ou contrato de parceria,, sendo a responsabilidade única e exclusivamente dos envolvidos, desde já eximindo a M2U de qualquer responsabilidade sob demandas desta natureza.</p>
+            <h5>3- Promoções e Campanhas:</h5>
+            <p>3.1- Campanha de Lançamento "Promoção Corretor Forte"</p>
+            <p>Objeto da Promoção: Durante o período de lançamento da plataforma, pelo prazo determinado de 90 (noventa) dias a contar da [Data de Início da Campanha / ou: da data de abertura do cadastro], os Usuários que aderirem ao Plano Básico usufruirão de uma condição especial e temporária: o direito de cadastrar e anunciar imóveis ilimitados na plataforma, mantendo o valor promocional de R$ 19,90 (dezenove reais e noventa centavos) mensais.</p>
+            <p>Limitação Padrão do Plano: O Usuário declara estar ciente de que a característica original e padrão do "Plano Básico" prevê o limite máximo de 20 (vinte) imóveis cadastrados simultaneamente. Portanto, a condição de "anúncios ilimitados" descrita no item anterior trata-se exclusivamente de uma bonificação temporária de lançamento.</p>
+            <p>Encerramento da Campanha e Transição: Findo o prazo de 90 (noventa) dias da campanha, a Plataforma aplicará a sua estratégia de crescimento e reestruturação de planos, ocorrendo o seguinte:</p>
+            <p>a) O "Plano Básico" retornará automaticamente à sua franquia original de 20 imóveis, mantendo-se o valor vigente da assinatura para esta modalidade.</p>
+            <p>b) Caso o Usuário possua mais de 20 imóveis ativos ao término da promoção, ele deverá adequar seus anúncios ao limite do plano ou, alternativamente, migrar para o novo plano com anúncios ilimitados, mediante o pagamento do valor correspondente à nova tabela de preços a ser divulgada pela Plataforma.</p>
+            <p>A Plataforma compromete-se a notificar os Usuários participantes com antecedência mínima de 15 (quinze) dias sobre o término da promoção e as instruções para migração ou adequação de anúncios.</p>
+            <h5>4- Pagamento:</h5>
+            <p>4.1- Ao contratar o plano e demais funcionalidades adicionais do MorarVip, Você tem conhecimento e concorda que deverá pagar os valores estabelecidos na Proposta de Plano. A cobrança será realizada pela M2U com base nas informações de pagamento fornecidas por Você. Assim, Você declara que: </p>
+            <p>As informações de faturamento enviadas para a M2U para os fins destes T&C estão corretas, incluindo seu número de cartão de crédito, data de validade, endereço de faturamento, inscrição no CNPJ ou no CPF, dados de conta bancária e qualquer outro dado aplicável ao meio de pagamento selecionado por Você.</p>
+            <p>Autoriza a M2U a utilizar os meios de pagamento informados por Você para realizar a cobrança dos planos e das funcionalidades adicionais eventualmente contratadas.</p>
+            <p>4.2 As funcionalidades utilizadas por Você na Plataforma MorarVip poderão ensejar a cobrança de tributos. Você reconhece que deverá realizar o pagamento dos tributos que legalmente for obrigado a pagar, incluindo aqueles que devem ser retidos (descontados) antes do pagamento a M2U. Nesses casos, Você deve apresentar a M2U a comprovação do pagamento de referido tributo. A M2U somente se responsabiliza pelos tributos que legalmente é obrigada a pagar.  </p>
+            <p>4.3- A M2U poderá ceder livremente a terceiros os créditos que têm a receber de Você em razão dos planos e demais funcionalidades da ferramenta MorarVip.</p>
+            <p>4.4- Caso os pagamentos não sejam feitos dentro do prazo acordado, Você estará sujeito a: </p>
+            <p>Inscrição do seu nome junto ao Serviço de Proteção ao Crédito SPC, SERASA</p>
+            <p>Aplicação de encargos moratórios, correspondentes a multa de 2% (dois por cento), juros e mora de 1% (um por cento) ao mês e correção monetária pelo índice central de preços - mercado (IGPM-FGV) ou outro índice que venha a substituí-lo sem prejuízo ao valor devido.</p>
+            <p>Suspensão na veiculação do seu plano sem aviso prévio.</p>
+            <p>Em caso de cobrança judicial do valor em aberto (débito), será aplicado o encargo adicional e 20% correspondente aos honorários advocatícios.</p>
+            <h5>5. Cobrança:</h5>
+            <p>5.1- Ao contratar os serviços da ferramenta MorarVip você declara estar de pleno acordo em receber cobranças nos meios de comunicação informados no seu cadastro, como e-mail, telefone de contato e aplicativos de mensagens.</p>
+            <p>5.2- Declara concordar como suficientes para fins de mora a comunicação de cobrança encaminhadas para os meios de comunicação registrados em seu cadastro de usuário, comprometendo-se a mantê-los rigorosamente atualizados e ativos.</p>
+        </div>
+    </div>
+    <h3 class="mt-3">6. Contato</h3>
     <p>Se você tiver alguma dúvida sobre estes termos de uso, entre em contato conosco através do nosso formulário de contato ou pelo e-mail fornecido em nosso site.</p>
     <p>Obrigado por utilizar o morar.vip!</p>
 </div>
